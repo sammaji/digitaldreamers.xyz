096e4ce1a50f1585cb6fd7bb6298bf4707394be5
Samya brata Maji <samyabratamaji334@gmail.com>
2023-05-23 16:52:09 +0530

    chore: minor changes

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/Button";
 import { TypographyH1, TypographyP } from "@/components/Typography";
-import ImgWebsite from "@/components/assets/images/website.png"
+import ImgWebsite from "@/components/assets/images/website.png";
+import ImgMobile from "@/components/assets/images/mobile.jpg";
 import React from "react";
 
 export default function page() {
@@ -25,25 +26,38 @@ export default function page() {
         </div>
       </div>
 
-      <div className="h-[100vh] w-[100vw]">
-        <div className="absolute h-[100vh] w-[100vw] overflow-hidden bg-white"></div>
+      <div className="h-[100vh] w-[100vw] bg-white">
+        <div className="grid grid-cols-[2fr_1fr] h-[100vh] gap-16 p-24">
+          <div>
+            <TypographyH1 className="text-black">Mobile Apps</TypographyH1>
+            <TypographyP className="text-black">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui
+              neque praesentium ex, temporibus est nihil officia? A quod
+              doloribus debitis, voluptatem, veritatis eaque reprehenderit
+              corrupti maxime ea tempore quam perferendis!
+            </TypographyP>
+          </div>
+
+          <div className="flex items-center justify-center">
+            <img src={ImgMobile.src} className="h-[400px] mobile-img" />
+          </div>
+        </div>
       </div>
 
       <div className="min-h-[100vh] h-auto w-[100vw]">
-          <div className="p-24">
-            <TypographyH1>Websites</TypographyH1>
-            <TypographyP className="pb-8">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-              fugit officiis cumque, doloremque repellendus nostrum! Impedit
-              velit exercitationem, beatae debitis quae inventore at in
-              voluptates neque accusantium consequuntur ab numquam. Lorem ipsum
-              dolor sit, amet consectetur adipisicing elit. Suscipit accusantium
-              dignissimos eum ratione illum laudantium odit corrupti, ex ducimus
-              fuga vel iste maxime eveniet dolore optio facilis, voluptas,
-              assumenda impedit.
-            </TypographyP>
+        <div className="p-24">
+          <TypographyH1>Websites</TypographyH1>
+          <TypographyP className="pb-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            fugit officiis cumque, doloremque repellendus nostrum! Impedit velit
+            exercitationem, beatae debitis quae inventore at in voluptates neque
+            accusantium consequuntur ab numquam. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Suscipit accusantium dignissimos eum
+            ratione illum laudantium odit corrupti, ex ducimus fuga vel iste
+            maxime eveniet dolore optio facilis, voluptas, assumenda impedit.
+          </TypographyP>
 
-            <img src={ImgWebsite.src} />
+          <img src={ImgWebsite.src} />
         </div>
       </div>
     </>
