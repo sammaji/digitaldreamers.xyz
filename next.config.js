8f70bb8c2bf1264d6c1398607b25818b3b615080
Samya brata Maji <samyabratamaji334@gmail.com>
2023-05-28 12:21:43 +0530

    added githubuser remote pattern

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+          protocol: "https",
+          hostname: "github-production-user-asset-*.s3.amazonaws.com",
+          port: "",
+          pathname: "*/**",
+        }],
+      },
+}
 
 module.exports = nextConfig
