3f63796b697c18e6e0179a7975d495b3936e7fa9
Samya brata Maji <samyabratamaji334@gmail.com>
2023-06-05 22:16:14 +0530

    improved csp

--- next.config.js
+++ next.config.js
@@ -1,18 +1,47 @@
+const ContentSecurityPolicy = `
+  enforce 'strict';
+  script-src 'self';
+  object-src 'self';
+  style-src 'self';
+  font-src 'self';
+  base-uri 'self';
+  `;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-          protocol: "https",
-          hostname: "github-production-user-asset-*.s3.amazonaws.com",
-          port: "",
-          pathname: "*/**",
-        }, {
-          protocol: "https",
-          hostname: "picsum.photos",
-          port: "",
-          pathname: "*/**"
-        }],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github-production-user-asset-*.s3.amazonaws.com",
+        port: "",
+        pathname: "*/**",
       },
-}
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "*/**",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g).trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
