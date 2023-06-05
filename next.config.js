eeb96b56e1b4e7be6ae581c22b78620a547bf82a
Samya brata Maji <samyabratamaji334@gmail.com>
2023-06-05 22:35:05 +0530

    removed remote patterns

--- next.config.js
+++ next.config.js
@@ -2,22 +2,6 @@ const ContentSecurityPolicy = `default-src https: 'unsafe-eval' 'unsafe-inline';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "github-production-user-asset-*.s3.amazonaws.com",
-        port: "",
-        pathname: "*/**",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "*/**",
-      },
-    ],
-  },
   async headers() {
     return [
       {
