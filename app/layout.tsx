9ec38ad028017878f1a8444d78dd141c2133e613
Samya brata Maji <samyabratamaji334@gmail.com>
2023-06-10 12:47:00 +0530

    seo: updates og:image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import OgImage from "@/assets/images/logo_digital_dreamers.png";
+import OgImage from "@/public/logo_digital_dreamers.png";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,9 +12,6 @@ export const metadata = {
     "Transform your digital presence with our expert website development and digital marketing solutions. We specialize in helping businesses elevate their online visibility and drive success in today's competitive landscape.",
   keywords:
     "website, digital marketing, ui ux, electron applications, desktop application, mobile app, software",
-  image: OgImage.src,
-  ["image:height"]: "500px",
-  ["image:width"]: "500px",
 };
 
 export default function RootLayout({
@@ -23,6 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          property="og:image"
+          content="https://github-production-user-asset-6210df.s3.amazonaws.com/116789799/244854298-ad63829a-226e-4c6a-825a-2c9fefc2adfd.png"
+        />
+      </Head>
       <body className={`${inter.className} overflow-x-hidden scroll-smooth`}>
         {children}
       </body>
